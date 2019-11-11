69a8fe141e1b48077c5e7eff45508dbfc3ac547b
azd1997 <374192922@qq.com>
2019-11-11 03:23:22 -0500

    20191111-lt242-1

---
+++ lttemplate/doc.go
@@ -0,0 +1,2 @@
+// Package lttemplate 放置一些算法的实现模板
+package lttemplate
---
+++ lttemplate/search/DFS.go
@@ -0,0 +1,15 @@
+package search
+
+// DFS 深度优先搜索模板(伪代码)，递归实现
+//func DFS(node *TreeNode) {
+//	if node in visited {
+//		// 已经访问过
+//		return
+//	}
+//
+//	visted.add(node)
+//
+//	// 处理当前节点
+//	DFS(node.left)
+//	DFS(node.right)
+//}
