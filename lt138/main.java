bf10022a54d4f20565b94abd2149170736aeea8a
azd1997 <374192922@qq.com>
2020-02-05 10:23:07 -0500

    20200205-lt148-138-1

--- lt138/main.java
+++ lt138/main.java
@@ -25,8 +25,31 @@ class Node {
     }
 }
 
+// 回溯解法
 class Solution {
-    public Node copyRandomList(Node head) {
 
+    // 哈希表记录旧节点到新节点的映射
+    HashMap<Node, Node> visited = new HashMap<Node, Node>();
+
+    public Node copyRandomList(Node head) {
+        // 递归停止条件
+        if (head == null) return null;
+
+        // 如果已经拷贝过(访问过)该节点则直接将该节点的那一份拷贝返回
+        if (this.visited.containsKey(head)) {
+            return this.visited.get(head);
+        }
+
+        // 创造一个新节点用于拷贝旧节点
+        Node node = new Node(head.val);
+
+        // 将 旧->新 节点映射存入 visited
+        this.visited.put(head, node);
+
+        // 递归调用
+        node.next = copyRandomList(head.next);
+        node.random = copyRandomList(head.random);
+
+        return node;
     }
 }
--- lt148/1.go
+++ lt148/1.go
@@ -3,4 +3,81 @@ package lt148
 
 // 排序链表
 
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
 
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+
+
+// 归并排序 非递归 自底向上迭代
+// O(nlogn)/O(1)
+func sortList(head *ListNode) *ListNode {
+	var h, h1, h2, pre, res *ListNode
+	h = head
+
+	// 求链表长度
+	n := 0
+	for h!=nil {
+		h = h.Next
+		n++
+	}
+
+	//
+	intv :=  1
+	res = &ListNode{}
+	res.Next = head
+	for intv < n {
+		pre = res
+		h = res.Next
+		for h!=nil {
+			//
+			i := intv
+			h1 = h
+			for i>0 && h!=nil {
+				h = h.Next
+				i--
+			}
+			if i>0 {break}
+			i = intv
+			h2 = h
+			for i>0 && h!=nil {
+				h = h.Next
+				i--
+			}
+
+			//
+			c1, c2 := intv, intv - i
+			for c1>0 && c2>0 {
+				if h1.Val < h2.Val {
+					pre.Next = h1
+					h1 = h1.Next
+					c1--
+				} else {
+					pre.Next = h2
+					h2 = h2.Next
+					c2--
+				}
+				pre = pre.Next
+			}
+			if c1==0 {
+				pre.Next = h2
+			} else {pre.Next = h1}
+
+			for c1>0 || c2>0 {
+				pre = pre.Next
+				c1--; c2--
+			}
+			pre.Next = h
+		}
+		intv *= 2
+	}
+	return res.Next
+}
