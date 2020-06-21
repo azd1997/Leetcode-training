0df1ce8dfdce070df6a3afd6624275fddeec9299
azd1997 <374192922@qq.com>
2020-06-21 09:39:10 +0800

    lcof56-I

---
+++ lcof56-I/1.cpp
@@ -0,0 +1,20 @@
+// 数组中数字出现的次数
+// 数组中有两个数字只出现1次，其它都出现两次，找出这两个数字
+// 要求O(n)/O(1) 
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> singleNumbers(vector<int>& nums) {
+
+    }
+};
+
+int main() {
+	cout << "111" ;
+    cout << "hello world";
+    return 0;
+}
